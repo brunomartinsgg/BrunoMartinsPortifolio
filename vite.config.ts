cc6892b7a0f4f5ec81b1479b5699ae27535ad153
Bruno Martins <brunnogomesgg@gmail.com>
2025-12-11 22:29:53 -0300

    depoly ok

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/BrunoMartinsPortifolio/", // 👈 Nome EXATO do repositório
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(), // funciona só no dev
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  server: {
+    port: 5173,
   },
 }));
